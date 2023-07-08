02205ede729b52dca81ff42acc67b7ec0045fb57
gamanedy144 <gamanedy@yahoo.com>
2023-07-08 18:54:44 +0300

    structure packages accordingly and create Controller, Repository, Service, and Entity classes

--- src/main/java/com/learning/freecodecamp/movies/controller/MovieController.java
+++ src/main/java/com/learning/freecodecamp/movies/controller/MovieController.java
@@ -5,7 +5,6 @@ import com.learning.freecodecamp.movies.service.MovieService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/learning/freecodecamp/movies/repository/MovieRepository.java
+++ src/main/java/com/learning/freecodecamp/movies/repository/MovieRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-    public Optional<Movie> findMovieByImdbId(String id);
+    Optional<Movie> findMovieByImdbId(String id);
 }
